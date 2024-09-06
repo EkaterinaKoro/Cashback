690722cd3cbc611d2618e8dd39985496210ba422
Kate <cimonyu@mail.ru>
2024-09-06 20:12:06 +0300

    Редокатирование кода

--- src/test/java/CashbackTest.java
+++ src/test/java/CashbackTest.java
@@ -7,12 +7,23 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class CashbackTest {
 
     @Test
-    void  StatusRemainCashback() {
+    void  StatusRemainCashback0() {
         Cashback cashback = new Cashback();
         int amount = 1000;
         int actual = cashback.remain(amount);
         int expected = 0;
         assertEquals(expected, actual);
+
+    }
+    @Test
+
+        public void shouldReturnRemainValue450() {
+            Cashback service = new Cashback();
+            int amount = 4500;
+            int actual = service.remain(amount);
+            int expected = 450;
+            assertEquals(actual, expected);
+        }
 
 
     }
