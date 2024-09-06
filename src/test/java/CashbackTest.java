552328a0de9e6a155a9a7f5692207bafc44be5d4
Kate <cimonyu@mail.ru>
2024-09-06 21:41:45 +0300

    JUnit

--- src/test/java/CashbackTest.java
+++ src/test/java/CashbackTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class CashbackTest {
 
     @Test
-    void  StatusRemainCashback0() {
+    void StatusRemainCashback0() {
         Cashback cashback = new Cashback();
         int amount = 1000;
         int actual = cashback.remain(amount);
@@ -15,17 +15,18 @@ class CashbackTest {
         assertEquals(expected, actual);
 
     }
+
     @Test
 
-        public void shouldReturnRemainValue450() {
-            Cashback service = new Cashback();
-            int amount = 4500;
-            int actual = service.remain(amount);
-            int expected = 450;
-            assertEquals(actual, expected);
-        }
-
-
+    public void shouldReturnRemainValue450() {
+        Cashback service = new Cashback();
+        int amount = 4500;
+        int actual = service.remain(amount);
+        int expected = 450;
+        assertEquals(actual, expected);
     }
+
+
+}
 
 }
